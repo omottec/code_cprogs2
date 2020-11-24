8b0dbb6d8158d9db4aba02107b3781883e079fee
omotec <omottec@gmail.com>
2020-11-24 23:46:30 +0800

    gcc & main

---
+++ pun.c
@@ -0,0 +1,13 @@
+//
+// Created by Jason Qin on 2020/11/24.
+//
+
+#include <stdio.h>
+
+//  gcc pun.c -o pun.o
+// ./pun.o
+int main(void)
+{
+    printf("To C, or not to c: that is the question.\n");
+    return 0;
+}
