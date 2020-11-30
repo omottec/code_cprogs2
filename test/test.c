55d43240700b78377f49461bfb665522b46eb42a
qinbingbing <qinbingbing@bytedance.com>
2020-11-30 16:12:43 +0800

    test scanf

---
+++ test/test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char ch;
+    printf("Enter first char: ");
+    scanf(" %c", &ch);
+    printf("First char: %c\n", ch);
+    printf("Enter second char: ");
+    scanf(" %c", &ch);
+    printf("Second char: %c\n", ch);
+}
